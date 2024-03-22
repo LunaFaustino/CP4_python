9595f4a312e05408a9e797468cb577e470e67768
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-22 12:02:41 -0300

    Busca com indice errado

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from algoritmos import ordenacao
 from testes import test_ordenacao
+from testes import test_busca
 
 
 dicionario = test_ordenacao.listas()
 for i in dicionario:
     test_ordenacao.teste_vetor(dicionario[i])
+
+numero = int(input("\nDigite o número a ser pesquisado: "))
+chave = input("""\nEm qual lista você deseja buscar:\n 
+        lista_ordenado_crescente
+        lista_nao_ordenado
+        lista_ordenado_decrescente
+        lista_vazio
+        lista_unico_elemento
+        lista_elementos_repetidos
+        lista_aleatoria10
+        lista_aleatoria100
+        lista_aleatoria1000""")
+
+if chave in dicionario:
+    resultado = test_busca.busca_numero(dicionario[chave], numero)
+else:
+    print("Chave inválida.")
+
 
 
 
--- testes/test_busca.py
+++ testes/test_busca.py
@@ -1,1 +1,18 @@
 from algoritmos import busca
+
+
+def busca_numero(vetor, numero):
+    print("LISTA: {vetor}")
+    indice_sequencial = busca.Busca.busca_sequencial(vetor, numero)
+    indice_binaria = busca.Busca.busca_binaria(vetor, numero)
+
+    if indice_sequencial != -1:
+        print(f"\n\nNA BUSCA SEQUENCIAL: \n O número {numero} foi encontrado no índice {indice_sequencial}.")
+    else:
+        print(f"\n\nNA BUSCA SEQUENCIAL: \n O número {numero} não foi encontrado.")
+
+    if indice_binaria != -1:
+        print(f"\n\nNA BUSCA BINÁRIA: \n O número {numero} foi encontrado no índice {indice_binaria}.")
+    else:
+        print(f"\n\nNA BUSCA BINÁRIA: \n O número {numero} não foi encontrado.")
+    return()
